576aef0d8eb607b5caddc53a0db747949dce459a
Guillaume Geoffroy <guillaume.geoffroy@epitech.eu>
2021-12-16 15:20:50 +0100

    fix: add ConditionToRespond

--- libs/api/index.js
+++ libs/api/index.js
@@ -5,6 +5,7 @@ function ApiHelper() {
 ApiHelper.prototype.component = require('./src/component');
 ApiHelper.prototype.AbstractService = require('./src/abstract.service');
 
+ApiHelper.prototype.ConditionToRespond = require('./src/helper/response');
 ApiHelper.prototype.Interface = require('./src/helper/interface');
 ApiHelper.prototype.methods = require('./src/helper/methods');
 module.exports = new ApiHelper();
